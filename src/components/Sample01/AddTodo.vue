7725d41126f950b7fd1d4123052b8cd18d776c26
nasset-nakamura <nakamura@n-asset.com>
2021-05-14 16:10:43 +0900

    [add]親子コンポーネントサンプルを追加

---
+++ src/components/Sample01/AddTodo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>
+      <span>userId: </span>
+      <input v-model="addItem.userId" type="text" />
+    </div>
+    <div>
+      <span>title: </span>
+      <input v-model="addItem.title" type="text" />
+    </div>
+    <div><button @click="add">Add</button></div>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["add"],
+  data() {
+    return {
+      addItem: {
+        userId: null,
+        title: "",
+      },
+    };
+  },
+  methods: {
+    add() {
+      this.$emit("add", this.addItem);
+      this.addItem.userId = null;
+      this.addItem.title = "";
+    },
+  },
+};
+</script>
---
+++ src/pages/sample01/index.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="container">
+    <Logo />
+    <h1 class="title">Sample01</h1>
+    <h2>Todo List</h2>
+    <div><input v-model="activeFlag" type="checkbox" /> active task only（count：{{ displayTodosCount }}）</div>
+    <table style="margin: 0 auto">
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>userId</th>
+          <th>title</th>
+          <th>completed</th>
+          <th>manage</th>
+        </tr>
+      </thead>
+      <tbody>
+        <sample-01-display-todo
+          v-for="todo in displayTodos"
+          :key="todo.id"
+          :todo="todo"
+          @toggle="toggleTodo(todo.id)"
+          @remove="removeTodo(todo.id)"
+        />
+      </tbody>
+    </table>
+    <hr />
+    <h2>Add Todo</h2>
+    <sample-01-add-todo @add="addTodo" />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const response = await $axios.get("https://jsonplaceholder.typicode.com/todos");
+    return { todos: response.data };
+  },
+
+  data() {
+    return {
+      activeFlag: true,
+    };
+  },
+
+  head() {
+    return {
+      title: "Sample01",
+    };
+  },
+
+  computed: {
+    displayTodos() {
+      if (this.activeFlag) {
+        return this.todos.filter((todo) => todo.completed === false);
+      } else {
+        return this.todos;
+      }
+    },
+    displayTodosCount() {
+      return this.displayTodos.length;
+    },
+  },
+
+  mounted() {
+    console.log({ todos: this.todos });
+  },
+
+  methods: {
+    addTodo(addItem) {
+      const { userId, title } = addItem;
+      const id =
+        Math.max.apply(
+          null,
+          this.todos.map((todo) => todo.id)
+        ) + 1;
+      this.todos = [
+        ...this.todos,
+        {
+          userId,
+          id,
+          title,
+          completed: false,
+        },
+      ];
+    },
+
+    toggleTodo(id) {
+      this.todos.forEach((todo) => {
+        if (todo.id === id) todo.completed = !todo.completed;
+      });
+    },
+
+    removeTodo(id) {
+      const check = confirm("削除します。よろしいですか？");
+      if (check) {
+        this.todos = this.todos.filter((todo) => todo.id !== id);
+      }
+    },
+  },
+};
+</script>
